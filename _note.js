df325743f9cfc3964cc6e601f5077bad2ab33f76
omarwebdeve <omarf977865@gmail.com>
2022-10-23 13:19:23 +0600

    vercel connect

--- _note.js
+++ _note.js
@@ -3,11 +3,20 @@
 // --------------------------
 // 1. goto node express (website)
 // 2. create folder name (cmd mkdir <file name>
+// 2.1 cd <folder name> goto previous folder
 // 3. npm init -y (-y if you have all default setup or you can setup strep by strep)
 // 4. create a entry index.js file 
 // 5. npm install express
 // 6. open hello world and some setting
 // 7. .............
-// 8. create a API
+// 8. create a API 
 // 9. API listen 
 // 10 sent request to path name
+
+// example 👉
+                //8
+                // const express = require("express")
+                //9
+                // const app = express()
+                //10
+                // app.listen(5000, () => console.log('server running is port 5000'))
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const categorize = require('./data/category.json')
+const news = require('./data/news.json')
 const app = express()
 const cors = require('cors')
 const port = process.env.port || 5000
@@ -10,7 +11,26 @@ app.get('/' , (req,res) => {
 })
 app.use(cors())
 app.get('/news-category' , (req,res) => {
-    res.send(categorize)
+        res.send(categorize)
+})
+app.get(`/news-category/:id` , (req,res) => {
+    const id = req.params.id
+    if (id === '08') {
+        res.send(news)
+    }else{
+        const newses = news.filter(c => c.category_id === id)
+        res.send(newses)
+    }
+})
+app.get('/news/:id' , (req,res) => {
+    const id = req.params.id
+    const selectNews = news.find(n => n._id === id)
+    res.send(selectNews)
+    // res.send(req.news.pram.id)
+})
+
+app.get('/news' , (req,res) => {
+    res.send(news)
 })
 
 
